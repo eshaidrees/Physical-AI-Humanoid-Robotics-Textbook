644cdafa25ead4a65689af3b074705e9ef373d6f
eshaidrees <160519993+eshaidrees@users.noreply.github.com>
2026-01-03 23:17:33 +0500

    update

--- frontend_book/docusaurus.config.js
+++ frontend_book/docusaurus.config.js
@@ -13,15 +13,17 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://eshaidrees.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub Pages: https://<USERNAME>.github.io/<REPO>/
-  baseUrl: '/ai-textbook/',
+  baseUrl: '/Physical-AI-Humanoid-Robotics-Textbook/',
+  trailingSlash: true,
+
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'ai-textbook', // Usually your GitHub org/user name.
-  projectName: 'ai-textbook', // Usually your repo name.
+  organizationName: 'eshaidrees', // Usually your GitHub org/user name.
+  projectName: 'Physical-AI-Humanoid-Robotics-Textbook', // Usually your repo name.
 
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
@@ -44,7 +46,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/ai-textbook/ai-textbook/tree/master/',
+            'https://github.com/eshaidrees/Physical-AI-Humanoid-Robotics-Textbook/tree/main/',
         },
         blog: false, // Optional: disable the blog plugin
         theme: {
@@ -73,7 +75,7 @@ const config = {
             label: 'Book',
           },
           {
-            href: 'https://github.com/ai-textbook/ai-textbook',
+            href: 'https://github.com/eshaidrees/Physical-AI-Humanoid-Robotics-Textbook/',
             label: 'GitHub',
             position: 'right',
           },
@@ -87,19 +89,19 @@ const config = {
             items: [
               {
                 label: 'ROS 2 - Robotic Nervous System',
-                to: '/docs/module-1-ros/index',
+                to: '/docs/module-1-ros/',
               },
               {
                 label: 'Digital Twin - Gazebo & Unity',
-                to: '/docs/module-2-simulation/index',
+                to: '/docs/module-2-simulation/',
               },
               {
                 label: 'AI Robot Brain - NVIDIA Isaac',
-                to: '/docs/module-3-ai/index',
+                to: '/docs/module-3-ai/',
               },
               {
                 label: 'Vision-Language-Action (VLA)',
-                to: '/docs/module-4-vla/index',
+                to: '/docs/module-4-vla/',
               },
             ],
           },
